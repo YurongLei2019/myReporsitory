3d756cc2c15b7e5bfbb54829c513b4e865fc79b8
YurongLei2019 <13595672347@163.com>
2020-05-21 16:11:01 +0800

    3提交
    4测试
    
    测试5
    
    (cherry picked from commit 26a07d8d5ff43f853eabceb9de88d74f339c8914)

---
+++ T.java
@@ -0,0 +1,12 @@
+public class T{
+
+
+  public static void main(String[]args){
+
+        SyStem.out.println("Java app");
+        SyStem.out.println("Java app");
+        SyStem.out.println("Java app");
+
+  }
+
+}
